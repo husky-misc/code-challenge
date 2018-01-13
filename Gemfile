644b241d0eccd0df01d6397137343190068f7064
Matheus Bernardo <matheussbernardo@gmail.com>
2018-01-13 16:50:05 -0200

    Creating pagination on compute history endpoint

--- Gemfile
+++ Gemfile
@@ -18,7 +18,8 @@ gem 'puma', '~> 3.7'
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
-
+gem 'will_paginate'
+gem 'pager_api'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- app/controllers/computes_history_controller.rb
+++ app/controllers/computes_history_controller.rb
@@ -1,13 +1,36 @@
 class ComputesHistoryController < ApplicationController
   # GET /compute_history
   def index
-    raw_compute_history = ComputeHistory.all
-    compute_history_log = []
-    
-    raw_compute_history.each do |log|
-      compute_history_log << JSON.parse(log.result)
+
+    if params[:page] && params[:page][:number]
+      raw_compute_history = ComputeHistory.paginate(page: params[:page][:number], per_page: 5)
+      compute_history_log = []
+
+      raw_compute_history.each do |log|
+        compute_history_log << JSON.parse(log.result)
+      end
+
+      current_page = params[:page][:number]
+    else
+      raw_compute_history = ComputeHistory.all
+      compute_history_log = []
+
+      raw_compute_history.each do |log|
+        compute_history_log << JSON.parse(log.result)
+      end
+
+      current_page = 0
     end
 
-    render json: compute_history_log
+    total_pages = (ComputeHistory.count / 5.0).ceil
+    pagination = {
+      "first_page": 1,
+      "current_page": current_page.to_i,
+      "last_page": total_pages,
+      "next_page_url": "http://localhost:3000/compute_history?page[number]=#{current_page.to_i+1}",
+      "prev_page_url": "http://localhost:3000/compute_history?page[number]=#{current_page.to_i-1}"
+    }
+    
+    render json: {history: compute_history_log, pagination: pagination}
   end
 end
