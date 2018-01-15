6f068e9aeb68bf4a33206238c77baf65a2378f35
Matheus Bernardo <matheussbernardo@gmail.com>
2018-01-15 14:17:44 -0200

    Testing Ip model

--- test/models/ip_test.rb
+++ test/models/ip_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class IpTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @ip = Ip.create(address: "127.0.0.1")
+    @ip.numbers = [Number.create(value: 9), Number.create(value: 1), Number.create(value: 3)]
+  end
+
+  test "should print numbers_value correct" do
+    numbers_value = @ip.numbers_value
+    assert_equal([1,3,9], numbers_value)
+  end
+
 end
