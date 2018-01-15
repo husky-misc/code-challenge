9b47ae37818b3ab60134bebfdbdf9ec656909cff
Matheus Bernardo <matheussbernardo@gmail.com>
2018-01-15 17:35:32 -0200

    Changing to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180113015506) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "compute_histories", force: :cascade do |t|
     t.text "result"
     t.datetime "created_at", null: false
@@ -28,8 +31,9 @@ ActiveRecord::Schema.define(version: 20180113015506) do
     t.integer "value"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "ip_id"
+    t.bigint "ip_id"
     t.index ["ip_id"], name: "index_numbers_on_ip_id"
   end
 
+  add_foreign_key "numbers", "ips"
 end
