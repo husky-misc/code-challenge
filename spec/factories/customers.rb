a14496b1a34af2769abddc2923a7455c682fc1ad
Will Douglas <willdowglas@gmail.com>
2020-12-30 20:07:52 +0000

    changing gem faker with ffaker

--- spec/factories/customers.rb
+++ spec/factories/customers.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :customer do
-    first_name { Faker::Name.first_name }
-    last_name { Faker::Name.last_name }
+    first_name { FFaker::Name.first_name }
+    last_name { FFaker::Name.last_name }
   end
 end
