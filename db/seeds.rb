b7a8cd9e2633e256088366ef87b91e1525c85594
Will Douglas <willdowglas@gmail.com>
2021-01-01 21:40:30 +0000

    updating seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,13 @@ customers.each do |customer|
     last_name: customer[:last_name]
   )
 
-  FactoryBot.create(:account, customer: customer)
+  account = FactoryBot.create(:account, customer: customer)
+
+  FactoryBot.create(
+    :credit_card,
+    account: account,
+    spent_limit: 10_000_00
+  )
 end
 
 puts '>>> customers and accounts created successfully!'
@@ -26,110 +32,63 @@ puts '    | which 10 are successful transactions'
 
 puts '    |    | 5 for Customer 1'
 5.times do
-  credit_card = FactoryBot.create(
-    :credit_card,
-    account: Account.find(1),
-    spent_limit: rand(1_000_00..10_000_00)
-  )
-  transaction = FactoryBot.build(
+  FactoryBot.build(
     :transaction,
-    credit_card: credit_card,
-    amount: rand(100_00..1_000_00)
-  )
-
-  transaction.pay!
+    credit_card: CreditCard.find(1),
+    amount: 1_000_00
+  ).pay!
 end
 
 puts '    |    | 3 for Customer 2'
 3.times do
-  credit_card = FactoryBot.create(
-    :credit_card,
-    account: Account.find(2),
-    spent_limit: rand(1_000_00..10_000_00)
-  )
-  transaction = FactoryBot.build(
+  FactoryBot.build(
     :transaction,
-    credit_card: credit_card,
-    amount: rand(100_00..1_000_00)
-  )
-
-  transaction.pay!
+    credit_card: CreditCard.find(2),
+    amount: 1_000_00
+  ).pay!
 end
 
 puts '    |    | 1 for Customer 3'
-credit_card = FactoryBot.create(
-  :credit_card,
-  account: Account.find(3),
-  spent_limit: rand(1_000_00..10_000_00)
-)
-transaction = FactoryBot.build(
+FactoryBot.build(
   :transaction,
-  credit_card: credit_card,
-  amount: rand(100_00..1_000_00)
-)
-transaction.pay!
+  credit_card: CreditCard.find(3),
+  amount: 1_000_00
+).pay!
 
 puts '    |    | 1 for Customer 4'
-credit_card = FactoryBot.create(
-  :credit_card,
-  account: Account.find(4),
-  spent_limit: rand(1_000_00..10_000_00)
-)
-transaction = FactoryBot.build(
+FactoryBot.build(
   :transaction,
-  credit_card: credit_card,
-  amount: rand(100_00..1_000_00)
-)
-transaction.pay!
-puts '    | 10 transactions created successfully!'
-puts '    |'
+  credit_card: CreditCard.find(4),
+  amount: 1_000_00
+).pay!
+
 
 puts '    | which 5 are failed transactions'
 puts '    |    | 3 for Customer 3'
 3.times do
-  credit_card = FactoryBot.create(
-    :credit_card,
-    account: Account.find(3),
-    spent_limit: rand(1_000_00..10_000_00)
-  )
-  transaction = FactoryBot.build(
+  FactoryBot.build(
     :transaction,
-    credit_card: credit_card,
-    amount: credit_card.spent_limit + 1_00
-  )
-
-  transaction.pay!
+    credit_card: CreditCard.find(3),
+    amount: 10_000_00
+  ).pay!
 end
+
 puts '    |    | 2 for Customer 4'
 2.times do
-  credit_card = FactoryBot.create(
-    :credit_card,
-    account: Account.find(4),
-    spent_limit: rand(1_000_00..10_000_00)
-  )
-  transaction = FactoryBot.build(
+  FactoryBot.build(
     :transaction,
-    credit_card: credit_card,
-    amount: credit_card.spent_limit + 1_00
-  )
-
-  transaction.pay!
+    credit_card: CreditCard.find(4),
+    amount: 10_000_00
+  ).pay!
 end
-puts '    | 5 transactions created successfully!'
-puts '    |'
 
 puts '    | which 5 are disputed transactions'
 puts '    |    | 3 for Customer 1'
 3.times do
-  credit_card = FactoryBot.create(
-    :credit_card,
-    account: Account.find(1),
-    spent_limit: rand(1_000_00..10_000_00)
-  )
   transaction = FactoryBot.build(
     :transaction,
-    credit_card: credit_card,
-    amount: rand(100_00..1_000_00)
+    credit_card: CreditCard.find(1),
+    amount: 1_000_00
   )
 
   transaction.pay!
@@ -137,20 +96,15 @@ puts '    |    | 3 for Customer 1'
 end
 puts '    |    | 2 for Customer 2'
 2.times do
-  credit_card = FactoryBot.create(
-    :credit_card,
-    account: Account.find(2),
-    spent_limit: rand(1_000_00..10_000_00)
-  )
   transaction = FactoryBot.build(
     :transaction,
-    credit_card: credit_card,
-    amount: rand(100_00..1_000_00)
+    credit_card: CreditCard.find(2),
+    amount: 1_000_00
   )
 
   transaction.pay!
   transaction.dispute!
 end
-puts '    | 5 transactions created successfully!'
+
 puts '########################################################################'
 puts '>>> seed finished successfully!'
