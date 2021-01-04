bfbe86c2eebc0583c1d0bad026a50ab9b72c0b76
PedroHamzehT <pedrohamzeh@gmai.com>
2021-01-04 17:27:53 -0300

    Generate schema file

---
+++ db/schema.rb
@@ -0,0 +1,54 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_01_04_202641) do
+
+  create_table "accounts", force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["customer_id"], name: "index_accounts_on_customer_id"
+  end
+
+  create_table "credit_cards", force: :cascade do |t|
+    t.integer "spent_limit"
+    t.string "customer_full_name"
+    t.string "number"
+    t.date "expiration_date"
+    t.string "cvv"
+    t.integer "account_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["account_id"], name: "index_credit_cards_on_account_id"
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.integer "credit_card_id", null: false
+    t.integer "amount"
+    t.string "currency"
+    t.string "status"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["credit_card_id"], name: "index_transactions_on_credit_card_id"
+  end
+
+  add_foreign_key "accounts", "customers"
+  add_foreign_key "credit_cards", "accounts"
+  add_foreign_key "transactions", "credit_cards"
+end
