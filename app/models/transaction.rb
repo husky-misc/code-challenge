b965a40dc87b0103e240d22944b7cb3882137346
PedroHamzehT <pedrohamzeh@gmai.com>
2021-01-04 18:00:29 -0300

    Beginning to include aasm

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,5 +1,36 @@
 # frozen_string_literal: true
 
 class Transaction < ApplicationRecord
+  include AASM
+
+  before_save :set_status
+
   belongs_to :credit_card
+
+  aasm do
+    state :dispute, initial: true
+    state :paid, :failed, :refunded
+
+    event :pay do
+      transitions from: :dispute, to: :paid
+    end
+
+    event :refund do
+      transitions from: %i[dispute paid], to: :refunded
+    end
+
+    event :fail do
+      transitions from: :dispute, to: :failed
+    end
+
+    event :to_dispute do
+      transitions from: :paid, to: :dispute
+    end
+  end
+
+  private
+
+  def set_status
+    current_state
+  end
 end
