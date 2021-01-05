1d7785be0b3dbc3362689d8a6ca012eb71cbb039
ggstroligo <gutostroligo@gmail.com>
2021-01-05 20:00:31 -0300

    Adding relationship from Customer with Account

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,5 @@
 class Customer < ApplicationRecord
   validates_presence_of :first_name, :last_name
+
+  has_one :account
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe Customer, type: :model do
     it { is_expected.to validate_presence_of(:first_name) }
     it { is_expected.to validate_presence_of(:last_name) }
   end
+
+  describe 'Relationships' do
+    it { is_expected.to have_one(:account) }
+  end
 end
