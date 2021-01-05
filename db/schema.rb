15463d2d28386a36e78428da4429818990cde427
Filipe Pereira <pereira.lipe96@gmail.com>
2021-01-05 16:51:49 -0300

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,47 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_01_05_165504) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "accounts", force: :cascade do |t|
+    t.bigint "customer_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["customer_id"], name: "index_accounts_on_customer_id"
+  end
+
+  create_table "credit_card_transactions", force: :cascade do |t|
+    t.bigint "credit_card_id", null: false
+    t.string "status", null: false
+    t.integer "amount", null: false
+    t.string "currency", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["credit_card_id"], name: "index_credit_card_transactions_on_credit_card_id"
+  end
+
+  create_table "credit_cards", force: :cascade do |t|
+    t.bigint "account_id", null: false
+    t.integer "spent_limit", null: false
+    t.string "number", null: false
+    t.string "expiration_date", null: false
+    t.string "cvv", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["account_id"], name: "index_credit_cards_on_account_id"
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "accounts", "customers"
+  add_foreign_key "credit_card_transactions", "credit_cards"
+  add_foreign_key "credit_cards", "accounts"
 end
