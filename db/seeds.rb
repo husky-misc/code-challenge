3fe0b320fd76ecf3f5fe196a2ae0631a103617c6
ggstroligo <gutostroligo@gmail.com>
2021-01-05 19:49:10 -0300

    Adding Customer seeds and factory

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+customers = [
+  { first_name: 'Yukihiro', last_name: 'Matsumoto' },
+  { first_name: 'Sandi', last_name: 'Matz' },
+  { first_name: 'Martin', last_name: 'Fowler' },
+  { first_name: 'Dr. Alan', last_name: 'Kay' }
+]
+
+puts '########################################'
+puts '>>> Adding new Customers to the system'
+
+customers.each do |customer|
+  FactoryBot.create(:customer, first_name: customer[:first_name], last_name: customer[:last_name])
+end
--- spec/factories/customers.rb
+++ spec/factories/customers.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :customer do
-    first_name { "MyString" }
-    last_name { "MyString" }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
   end
 end
