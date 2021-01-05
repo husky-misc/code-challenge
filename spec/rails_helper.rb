2795a5adc05b22a886113a41ddd45b25794964c8
ggstroligo <gutostroligo@gmail.com>
2021-01-05 19:38:40 -0300

    Setup shoulda matchers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,7 +11,6 @@ require 'rspec/rails'
 require 'database_cleaner'
 require 'support/factory_bot'
 
-
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -65,4 +64,11 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end
