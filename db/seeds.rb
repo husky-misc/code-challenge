b538314cd33674b43d15401c078457127bf2292d
PedroHamzehT <pedrohamzeh@gmai.com>
2021-01-07 18:04:38 -0300

    Starting to create the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,53 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+customer_one = Customer.create(first_name: 'Yukihiro', last_name: 'Matsumoto')
+customer_two = Customer.create(first_name: 'Sandi', last_name: 'Matz')
+customer_three = Customer.create(first_name: 'Martin', last_name: 'Fowler')
+customer_four = Customer.create(first_name: 'Dr. Alan', last_name: 'Kay')
+
+account_one = Account.create(customer_id: customer_one.id)
+account_two = Account.create(customer_id: customer_two.id)
+account_three = Account.create(customer_id: customer_three.id)
+account_four = Account.create(customer_id: customer_four.id)
+
+account_one = CreditCard.create(
+  account_id: account_one.id,
+  number: '4916793613502348',
+  cvv: '443',
+  spent_limite: 150000,
+  expiration_date: '2022/12/07',
+  flag: 'Visa',
+  customer_full_name: account_one.customer.full_name
+)
+
+account_two = CreditCard.create(
+  account_id: account_two.id,
+  number: '4929551440422139',
+  cvv: '322',
+  spent_limite: 250000,
+  expiration_date: '07/07/2021',
+  flag: 'Visa',
+  customer_full_name: account_two.customer.full_name
+)
+
+account_three = CreditCard.create(
+  account_id: account_three.id,
+  number: '5414651510520330',
+  cvv: '125',
+  spent_limite: 350000,
+  expiration_date: '2021/08/07',
+  flag: 'MasterCard',
+  customer_full_name: account_three.customer.full_name
+)
+
+account_four = CreditCard.create(
+  account_id: account_four.id,
+  number: '4916971365086203',
+  cvv: '881',
+  spent_limite: 450000,
+  expiration_date: '2021/08/07',
+  flag: 'Visa',
+  customer_full_name: account_four.customer.full_name
+)
