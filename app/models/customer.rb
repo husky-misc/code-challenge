983eaa390662c2e0cbb0ab24bac4e3f828e7de1a
PedroHamzehT <pedrohamzeh@gmai.com>
2021-01-07 17:57:20 -0300

    create full_name method in customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,4 +5,8 @@ class Customer < ApplicationRecord
   has_many :credit_cards, through: :account
 
   validates_presence_of :first_name, :last_name
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
