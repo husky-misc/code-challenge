5582be33b6abc9e177641f182afffc9894414fec
ggstroligo <gutostroligo@gmail.com>
2021-01-07 00:33:58 -0300

    Updating seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,16 @@
-customers = [
-  { first_name: 'Yukihiro', last_name: 'Matsumoto' },
-  { first_name: 'Sandi', last_name: 'Matz' },
-  { first_name: 'Martin', last_name: 'Fowler' },
-  { first_name: 'Dr. Alan', last_name: 'Kay' }
-]
+puts '####################'
+puts '||GENERATING SEEDS||'
+puts '####################'
 
-puts '########################################'
-puts '>>> Adding new Customers to the system'
+puts '>>> Creating customers and accounts'
+matz = FactoryBot.create(:customer, :with_account, first_name: 'Yukihiro', last_name: 'Matsumoto')
+sandi = FactoryBot.create(:customer, :with_account, first_name: 'Sandi', last_name: 'Matz')
+martin = FactoryBot.create(:customer, :with_account, first_name: 'Martin', last_name: 'Fowler')
+alan = FactoryBot.create(:customer, :with_account, first_name: 'Dr. Alan', last_name: 'Kay')
 
-customers.each do |customer|
-  FactoryBot.create(:customer, first_name: customer[:first_name], last_name: customer[:last_name])
-end
+puts '>>> Creating credit cards'
+FactoryBot.create(:credit_card, account: matz.account)
+FactoryBot.create(:credit_card, account: sandi.account)
+FactoryBot.create(:credit_card, account: martin.account)
+FactoryBot.create(:credit_card, account: alan.account)
+
