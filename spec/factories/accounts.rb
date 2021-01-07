4120ecdbd060f76cedb6375983d5b6eadcbff555
ggstroligo <gutostroligo@gmail.com>
2021-01-07 00:34:08 -0300

    Updating factories

--- spec/factories/accounts.rb
+++ spec/factories/accounts.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :account do
-    customer
+    association(:customer, factory: :customer)
   end
 end
--- spec/factories/customers.rb
+++ spec/factories/customers.rb
@@ -3,4 +3,10 @@ FactoryBot.define do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
   end
+
+  trait :with_account do
+    after(:create) do |customer|
+      FactoryBot.create(:account, customer: customer)
+    end
+  end
 end
