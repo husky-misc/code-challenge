e85583c7a762537effcaa5c9733e1e16899292c5
PedroHamzehT <pedrohamzeh@gmai.com>
2021-01-08 18:11:04 -0300

    Remove the unnecessaries DOs and whitespaces

--- app/controllers/api/v1/credit_cards_controller.rb
+++ app/controllers/api/v1/credit_cards_controller.rb
@@ -1,11 +1,11 @@
-module Api do
-  module V1 do
+module Api
+  module V1
     class CreditCardsController < ApplicationController
       def charge
         transaction = Transaction.new(charge_params)
         if transaction.save
           transaction_json = TransactionSerializer.new(transaction).serialized_json
-    
+
           render json: transaction_json, status: 200
         else
           render json: { error: transaction.errors.message }, status: 400
@@ -13,16 +13,16 @@ module Api do
       rescue StandardError => e
         render json: { error: e.message }, status: 500
       end
-    
+
       private
-    
+
       def set_credit_card
         @credit_card = CreditCard.find(params[:credit_card_id])
       end
-    
+
       def charge_params
         params.require(:transaction).permit(:amount, :currency)
       end
-    end 
+    end
   end
 end
