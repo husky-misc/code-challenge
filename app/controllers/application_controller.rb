ab5a4ebbd1387d6aaf496d8184b341ef27ada69c
ggstroligo <gutostroligo@gmail.com>
2021-01-08 00:42:10 -0300

    Rescuing RecordNotFound on ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  private
+
+  def not_found(message)
+    render json: { error: message }, status: 404
+  end
 end
