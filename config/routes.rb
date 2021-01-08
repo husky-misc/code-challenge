e6c260f8aae7b2fc67c8026350e99df7527e109a
PedroHamzehT <pedrohamzeh@gmai.com>
2021-01-08 18:15:58 -0300

    Fix the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  namespace :ap1 do
+  namespace :api do
     namespace :v1 do
-      get '/customer/:customer_id/credit_card/:credit_card_id/transactions', to: 'transactions#index'
-
-      post '/customer/:customer_id/credit_card/:credit_card_id/charge', to: 'credit_cards#charge'
+      get '/customers/:customer_id/credit_cards/:credit_card_id/transactions', to: 'transactions#index'
+      post '/customers/:customer_id/credit_cards/:credit_card_id/charge', to: 'credit_cards#charge'
     end
   end
 end
