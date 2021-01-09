76ac96ff0d94c58ed101e11b09c7af3344617b2b
ggstroligo <gutostroligo@gmail.com>
2021-01-09 02:57:48 -0300

    Remove named param from withdrawable concern

--- app/models/concerns/withdrawable.rb
+++ app/models/concerns/withdrawable.rb
@@ -2,7 +2,7 @@ module Withdrawable
   extend ActiveSupport::Concern
 
   included do
-    def withdraw!(amount:)
+    def withdraw!(amount)
       self.update!(spent_limit: self.spent_limit - amount.to_i)
     end
   end
--- spec/support/concerns/withdrawable.rb
+++ spec/support/concerns/withdrawable.rb
@@ -5,14 +5,14 @@ shared_examples_for 'withdrawable' do
 
   describe '.withdraw!' do
     let(:model) { create(described_class.to_s.underscore, spent_limit: spent_limit) }
-    subject { model.withdraw!(amount: amount) }
+    subject { model.withdraw!(amount) }
 
     let(:spent_limit) { 100_000_00 }
     let(:amount) { 50_000_00 }
 
     it { is_expected.to be_truthy }
     it 'is expected to withdraw the amount from limit' do
-      model.withdraw!(amount: amount)
+      model.withdraw!(amount)
       expect(model.spent_limit).to eq(spent_limit - amount)
     end
   end
