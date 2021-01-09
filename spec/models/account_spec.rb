d79d59b4f04594c5eeddaa6f9b43bb67572fd081
ggstroligo <gutostroligo@gmail.com>
2021-01-09 02:50:55 -0300

    Add case to account spec

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 RSpec.describe Account, type: :model do
   describe 'Relationships' do
     it { is_expected.to belong_to(:customer) }
+    it { is_expected.to have_many(:credit_cards) }
   end
 end
