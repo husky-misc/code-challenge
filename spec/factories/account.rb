32cccb9ae740c2f9dfe9113cd9342cfb735b53c5
PedroHamzehT <pedrohamzeh@gmai.com>
2021-01-10 12:17:37 -0300

    Fix problems with associations

--- spec/factories/account.rb
+++ spec/factories/account.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :account do
-    customer_id { build(:customer) }
+    customer_id { create(:customer).id }
   end
 end
--- spec/factories/credit_card.rb
+++ spec/factories/credit_card.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :credit_card do
-    account_id { build(:account) }
+    account_id { create(:account).id }
     number { Faker::Number.number(digits: 16) }
     cvv { rand(100..999) }
     expiration_date { (Time.now + rand(80000000..200000000)).strftime('%m/%y') }
-    customer_full_name { account.customer.full_name }
+    customer_full_name { Faker::Name.name }
     spent_limit { rand(100000..1000000) }
     flag { %w[Visa MasterCard].sample }
   end
