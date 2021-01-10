61bdfe8131458bdbabdbdf6c09b0717ae4ab4454
PedroHamzehT <pedrohamzeh@gmai.com>
2021-01-10 14:06:18 -0300

    Fix problem with expiration date

--- spec/factories/credit_card.rb
+++ spec/factories/credit_card.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     account_id { create(:account).id }
     number { Faker::Number.number(digits: 16) }
     cvv { rand(100..999) }
-    expiration_date { (Time.now + rand(80000000..200000000)).strftime('%y/%m/%d') }
+    expiration_date { (Time.now + rand(80000000..200000000)).strftime('%Y/%m/%d') }
     customer_full_name { Faker::Name.name }
     spent_limit { rand(100000..1000000) }
     flag { %w[Visa MasterCard].sample }
