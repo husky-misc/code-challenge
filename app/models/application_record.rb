6a7ff7a0c7cf605d5176ecdbadb61d2873c12133
Thiago Tenório <thiago.tenorio@rogalabs.com>
2021-01-12 17:39:48 -0300

    feat: added filter function

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,11 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.filter(params)
+    results = self.where(nil)
+    params.each do |key, value|
+      results = results.public_send(key, value) if value.present?
+    end
+    results
+  end
 end
