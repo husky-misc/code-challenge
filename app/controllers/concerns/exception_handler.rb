cb561e09ad981b43b37f6e222aabaead7d60446a
Thiago Tenório <thiago.tenorio@rogalabs.com>
2021-01-12 17:31:41 -0300

    feat: define and load ExceptionHandler module

---
+++ app/controllers/concerns/exception_handler.rb
@@ -0,0 +1,9 @@
+module ExceptionHandler
+  extend ActiveSupport::Concern
+
+  included do
+    rescue_from ActiveRecord::RecordNotFound do |e|
+      render json: { message: e.message }, status: :not_found
+    end
+  end
+end
