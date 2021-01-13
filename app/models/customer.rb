70123489a99bb47122e8ee099be0f6f741c67dd5
Thiago Tenório <thiago.tenorio@rogalabs.com>
2021-01-13 14:19:44 -0300

    feat(spec): test for customer/account relationship

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,3 @@
 class Customer < ApplicationRecord
+  has_one :account
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_one(:account) }
 end
