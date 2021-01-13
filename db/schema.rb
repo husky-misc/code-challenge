176c3ee5372386e4a0f1f0fec04c41e566a32c9e
Thiago Tenório <thiago.tenorio@rogalabs.com>
2021-01-13 14:15:12 -0300

    feat: create account model

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_13_170309) do
+ActiveRecord::Schema.define(version: 2021_01_13_171237) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "accounts", force: :cascade do |t|
+    t.bigint "customer_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["customer_id"], name: "index_accounts_on_customer_id"
+  end
 
   create_table "customers", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "accounts", "customers"
 end
---
+++ spec/factories/accounts.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :account do
+    customer { nil }
+  end
+end
