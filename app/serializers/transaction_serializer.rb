762b9ebabd085902d81ff49ab40aa5005cdaaf07
Thiago Tenório <thiago.tenorio@rogalabs.com>
2021-01-14 01:48:55 -0300

    docs: updated transaction serializer documentation

--- app/serializers/transaction_serializer.rb
+++ app/serializers/transaction_serializer.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: transactions
+#
+#  id             :bigint           not null, primary key
+#  amount         :float            not null
+#  currency       :string           not null
+#  status         :integer          default("dispute"), not null
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  credit_card_id :bigint           not null
+#
+# Indexes
+#
+#  index_transactions_on_credit_card_id  (credit_card_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (credit_card_id => credit_cards.id)
+#
 class TransactionSerializer < ActiveModel::Serializer
   attributes :id, :created, :status, :amount, :currency, :credit_card_id
 
