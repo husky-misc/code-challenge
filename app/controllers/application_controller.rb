753a60dba0d59d6e768912ddf13ffd20bc67e3e3
Thiago Tenório <thiago.tenorio@rogalabs.com>
2021-01-13 22:21:25 -0300

    feat: add pagination_info This function will serve to return the pagination metadata

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::API
   include ExceptionHandler
+
+  def pagination_info(collection)
+    { total_pages: collection.total_pages, total_count: collection.total_count }
+  end
 end
