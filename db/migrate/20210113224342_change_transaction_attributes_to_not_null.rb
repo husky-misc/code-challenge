90955e17b2bc5ea2e574db65b72f7b7b2570cebe
Thiago Tenório <thiago.tenorio@rogalabs.com>
2021-01-14 01:46:38 -0300

    fix: def up and down for irreversible migration

--- db/migrate/20210113224342_change_transaction_attributes_to_not_null.rb
+++ db/migrate/20210113224342_change_transaction_attributes_to_not_null.rb
@@ -1,7 +1,13 @@
 class ChangeTransactionAttributesToNotNull < ActiveRecord::Migration[6.0]
-  def change
+  def up
     change_column :transactions, :amount, :float, null: false
     change_column :transactions, :currency, :string, null: false
     change_column :transactions, :status, :integer, null: false
+  end
+
+  def down 
+    change_column :transactions, :status, :integer, null: true
+    change_column :transactions, :currency, :string, null: true
+    change_column :transactions, :amount, :float, null: true
   end
 end
