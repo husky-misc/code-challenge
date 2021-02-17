36b6c9c2f55c9b7584260414be578460b4493fa4
Nathaly Rocha <nathalyfroliveira@gmail.com>
2021-02-17 13:23:09 -0300

    Cria scores e verifica se partida já foi criada

--- app/services/extractor/extract_data.rb
+++ app/services/extractor/extract_data.rb
@@ -12,7 +12,7 @@ module Extractor
         if (line.match("New match"))
           match_data = Extractor::Match.new(line)
 
-          self.match = ::Match.create(
+          self.match = ::Match.find_or_create_by(
             match_id: match_data.id,
             start_date: match_data.start_date_to_datetime
           )
@@ -21,6 +21,19 @@ module Extractor
         if(line.match("ended"))
           match_data = Extractor::Match.new(line)
           self.match.update(end_date: match_data.end_date_to_datetime)
+        end
+
+        if (line.match("killed"))
+          score_data = Extractor::Score.new(line)
+          gun = ::Gun.find_or_create_by(name: score_data.gun)
+
+          if (score_data.killer != "<WORLD>")
+            killer = ::Player.find_or_create_by(name: score_data.killer)
+            ::Frag.create(player: killer, gun: gun, match: match, date: score_data.start_date_to_datetime)
+          end
+
+          victim = ::Player.find_or_create_by(name: score_data.victim)
+          ::Death.create(player: victim, gun: gun, match: match, date: score_data.start_date_to_datetime)
         end
       end
     end
