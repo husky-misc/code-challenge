608b30dd846588b19ce94e617fd2181adf19db9e
Nathaly Rocha <nathalyfroliveira@gmail.com>
2021-02-17 13:26:26 -0300

    Atualiza schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_17_041736) do
+ActiveRecord::Schema.define(version: 2021_02_17_154328) do
+
+  create_table "guns", force: :cascade do |t|
+    t.string "name"
+    t.string "string"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "matches", force: :cascade do |t|
     t.string "match_id"
@@ -22,4 +29,26 @@ ActiveRecord::Schema.define(version: 2021_02_17_041736) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "players", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "scores", force: :cascade do |t|
+    t.integer "player_id", null: false
+    t.integer "match_id", null: false
+    t.integer "gun_id", null: false
+    t.datetime "date"
+    t.string "type"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["gun_id"], name: "index_scores_on_gun_id"
+    t.index ["match_id"], name: "index_scores_on_match_id"
+    t.index ["player_id"], name: "index_scores_on_player_id"
+  end
+
+  add_foreign_key "scores", "guns"
+  add_foreign_key "scores", "matches"
+  add_foreign_key "scores", "players"
 end
