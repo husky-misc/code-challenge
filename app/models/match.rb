9481038381a5acfebc6e6e21db28e183e23a89e4
Nathaly Rocha <nathalyfroliveira@gmail.com>
2021-02-17 15:19:13 -0300

    Adiciona associação has_many e método ranking

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,5 +1,19 @@
 class Match < ApplicationRecord
-  validates :match_id, :start_date, presence: true
+  has_many :scores
+  has_many :players, through: :scores
+  has_many :frags
+  has_many :deaths
 
+  validates :match_id, :start_date, presence: true
   validates :match_id, uniqueness: true
+
+  def ranking
+    players.uniq.map do |player|
+      {
+        name: player.name,
+        frags: player.frags.where(match_id: id).size,
+        deaths: player.deaths.where(match_id: id).size
+      }
+    end.sort_by { |data| data[:frags] }.reverse
+  end
 end
