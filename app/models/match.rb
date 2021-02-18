4cd4dfe6cebdee805a19a10d2d0adc0f6f9fe9a1
Nathaly Rocha <nathalyfroliveira@gmail.com>
2021-02-18 00:53:35 -0300

    Adiciona ranking de 5 mortes em um minuto

--- app/models/match.rb
+++ app/models/match.rb
@@ -14,7 +14,8 @@ class Match < ApplicationRecord
         frags: player.frags_per_match(id).size,
         deaths: player.deaths_per_match(id).size,
         favorite_gun: player.favorite_gun(id)&.name,
-        award: player.deaths_per_match(id).empty?
+        award: player.deaths_per_match(id).empty?,
+        award_five_kills: player.five_kills_in_one_minute(id)
       }
     end.sort_by { |data| data[:frags] }.reverse
   end
--- app/models/player.rb
+++ app/models/player.rb
@@ -21,6 +21,15 @@ class Player < ApplicationRecord
       .order("guns DESC").first
   end
 
+  def five_kills_in_one_minute(match_id)
+    first_frag = frags_per_match(match_id).first
+    if first_frag
+      frags_per_match(match_id).where(date: first_frag.date..(first_frag.date + 1.minute)).size >= 5
+    else
+      false
+    end
+  end
+
   def self.global_ranking
     Player.all.map do |player|
       {
