45bbb6b57777383ae00d74849f9e58f31d09f94c
Chrystiam Andrade <chrystiam.andrade@gmail.com>
2021-02-21 12:40:03 -0300

    fix: change model associations

---
+++ app/models/gun.rb
@@ -0,0 +1,3 @@
+class Gun < ApplicationRecord
+    has_one :kill_log
+end
---
+++ db/schema.rb
@@ -0,0 +1,87 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_02_21_131738) do
+
+  create_table "awards", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "guns", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "kill_logs", force: :cascade do |t|
+    t.integer "match_id", null: false
+    t.integer "killer_id"
+    t.integer "killed_id"
+    t.integer "gun_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["gun_id"], name: "index_kill_logs_on_gun_id"
+    t.index ["match_id"], name: "index_kill_logs_on_match_id"
+  end
+
+  create_table "match_player_awards", force: :cascade do |t|
+    t.integer "match_id", null: false
+    t.integer "player_id", null: false
+    t.integer "award_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["award_id"], name: "index_match_player_awards_on_award_id"
+    t.index ["match_id"], name: "index_match_player_awards_on_match_id"
+    t.index ["player_id"], name: "index_match_player_awards_on_player_id"
+  end
+
+  create_table "match_players", force: :cascade do |t|
+    t.integer "match_id", null: false
+    t.integer "player_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "team_id", null: false
+    t.index ["match_id"], name: "index_match_players_on_match_id"
+    t.index ["player_id"], name: "index_match_players_on_player_id"
+    t.index ["team_id"], name: "index_match_players_on_team_id"
+  end
+
+  create_table "matches", force: :cascade do |t|
+    t.string "winner"
+    t.string "loser"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "players", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "kill_logs", "guns"
+  add_foreign_key "kill_logs", "matches"
+  add_foreign_key "match_player_awards", "awards"
+  add_foreign_key "match_player_awards", "matches"
+  add_foreign_key "match_player_awards", "players"
+  add_foreign_key "match_players", "matches"
+  add_foreign_key "match_players", "players"
+  add_foreign_key "match_players", "teams"
+end
