6ab09a839262cce9650969bc9af524362f30616c
Chrystiam Andrade <chrystiam.andrade@gmail.com>
2021-02-21 13:07:09 -0300

    fix: remove standard rails test_unit

--- test/models/gun_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class GunTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/kill_log_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class KillLogTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
