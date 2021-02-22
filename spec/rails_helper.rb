455d7d581554631532a1820f05da22bdea187435
Nathaly Rocha <nathalyfroliveira@gmail.com>
2021-02-22 17:53:42 -0300

    Adiciona rspec

---
+++ spec/rails_helper.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+abort('The Rails environment is running in production mode!') if Rails.env.production?
+require 'rspec/rails'
+require 'database_cleaner'
+require 'factory_bot_rails'
+
+ActiveRecord::Migration.maintain_test_schema!
+
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
+RSpec.configure do |config|
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+
+  config.include FactoryBot::Syntax::Methods
+end
