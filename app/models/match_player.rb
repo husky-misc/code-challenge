aad8046572e8c393621499a6c0e3a5e9f45464f3
Chrystiam Andrade <chrystiam.andrade@gmail.com>
2021-02-23 18:55:52 -0300

    fix: match players validation

--- app/models/match_player.rb
+++ app/models/match_player.rb
@@ -4,8 +4,18 @@ class MatchPlayer < ApplicationRecord
   belongs_to :team, optional: true
 
   validate :validate_max_number_of_players
+  
+  def self.ranking(id = nil)
+    if(id)
+      self.where(match_id: id)
+    else
+      self.all.distinct.pluck(:player_id)
+    end
+  end
 
   def validate_max_number_of_players
-    errors.add("Reached max players") if MatchPlayer.count == 20
+    errors.add("Reached max players") if MatchPlayer.where(match_id: self.match_id).count == 20
   end
+
+  
 end
