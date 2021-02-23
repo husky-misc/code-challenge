f115a7d8f078a15669138e08276e73919854d680
Chrystiam Andrade <chrystiam.andrade@gmail.com>
2021-02-23 18:54:47 -0300

    fix: routes for dashboard controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'dashboard/index'
+  get 'dashboard/show'
+  post 'dashboard/upload_file'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
