a21931044f4de47e31887e9535cdabab1b68659c
Andrew Oliveira <andrewhenriquef@gmail.com>
2021-02-23 21:01:48 -0300

    (+) Configure application TimeZone

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,7 @@ module CodeChallenge
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.time_zone = 'Brasilia'
+    config.active_record.default_timezone = :local
   end
 end
