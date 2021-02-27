29955b7170e59c778422e534270cc607e130b70b
Oswaldo Linhares <oswaldolinhares@gmail.com>
2021-02-27 14:03:30 -0300

    Configurate database_cleaner

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -21,4 +21,16 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+
+  # DatabaseCleaner
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
 end
