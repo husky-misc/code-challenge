fc477c20cebee679928c4df3e532d3c7596cbf68
Oswaldo Linhares <oswaldolinhares@gmail.com>
2021-02-27 14:51:03 -0300

    Add factory bot trait for attaching active storage

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
+FactoryBot::SyntaxRunner.class_eval do
+  include ActionDispatch::TestProcess
+  include ActiveSupport::Testing::FileFixtures
+end
+
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   config.before(:suite) do
