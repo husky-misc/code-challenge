0042e7997509b76d2fa0c50e9bdf09c27ba06171
Oswaldo Linhares <oswaldolinhares@gmail.com>
2021-03-02 20:04:35 -0300

    Fix log2 and include in the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
 game = Game.new
 game.file.attach(io: File.open("#{Rails.root}/spec/files/log.txt"), filename: 'log')
 game.save
+
+game = Game.new
+game.file.attach(io: File.open("#{Rails.root}/spec/files/log2.txt"), filename: 'log2')
+game.save
