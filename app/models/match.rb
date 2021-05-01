853f6bcc4e25419c6e376bb81e76b13d8e47e878
Alessandro Costa <alessandro.asac@gmail.com>
2021-05-01 17:34:38 -0300

    Added Match limit of 20 players

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,9 +1,17 @@
 class Match < ApplicationRecord
+  PLAYERS_LIMIT = 20
+
   include Importable
 
   has_many :kills
 
+  validates_with TotalPlayersLimitValidator
+  
   def rank
     RankQuery.new(self).call
+  end
+  
+  def players
+    (kills.map(&:killer) | kills.map(&:killed)).compact
   end
 end
--- test/models/match_test.rb
+++ test/models/match_test.rb
@@ -2,8 +2,16 @@ require "test_helper"
 
 class MatchTest < ActiveSupport::TestCase
   test ".import" do
-    assert_difference ->{ Match.count } => 3, ->{ Kill.count } => 9, ->{ Player.count } => 6, ->{ Weapon.count } => 2 do
+    assert_difference ->{ Match.count } => 4, ->{ Kill.count } => 22, ->{ Player.count } => 21, ->{ Weapon.count } => 2 do
       Match.import(file: file_fixture("matches"))
+    end
+  end
+
+  test ".import should fail" do
+    assert_difference ->{ Match.count } => 0, ->{ Kill.count } => 0, ->{ Player.count } => 0, ->{ Weapon.count } => 0 do
+      assert_raises ActiveRecord::RecordInvalid do
+        Match.import(file: file_fixture("invalid-matches"))
+      end
     end
   end
 end
