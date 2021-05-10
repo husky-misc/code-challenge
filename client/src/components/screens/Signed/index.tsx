8bf58073f6c7814088fabaec825b1f52143bc812
gideon-esl <gfernandes@eslsistemas.com.br>
2021-05-10 17:25:15 -0300

    feature: (frontend): Add Signed screen

--- client/src/components/screens/Signed/index.tsx
+++ client/src/components/screens/Signed/index.tsx
@@ -1,9 +1,21 @@
-import React from "react";
-
-import { Container } from "./styles";
+import { useHistory } from "react-router-dom";
+import { Button } from "../../atoms";
+import { Container, WhiteBlock, CompleteImage } from "./styles";
+import { IMAGES } from "../../../constants";
 
 const Signed: React.FC = () => {
-  return <Container>Signed</Container>;
+  const history = useHistory();
+
+  return (
+    <Container>
+      <WhiteBlock>
+        <CompleteImage src={IMAGES.complete} />
+        <h1>Registration Complete!</h1>
+        <p>You are now part of the Husky Gaming Platform!</p>
+        <Button text="Sign In" clickFunction={() => history.push("/")} />
+      </WhiteBlock>
+    </Container>
+  );
 };
 
 export default Signed;
--- client/src/components/screens/Signed/styles.ts
+++ client/src/components/screens/Signed/styles.ts
@@ -1,3 +1,42 @@
 import styled from "styled-components";
+import { COLORS } from "../../../constants";
 
-export const Container = styled.main``;
+export const Container = styled.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  height: 100vh;
+`;
+
+export const WhiteBlock = styled.section`
+  display: flex;
+  flex: 1;
+  justify-content: baseline;
+  align-items: center;
+  flex-direction: column;
+
+  max-width: 580px;
+  border-radius: 1.5rem;
+  background: ${COLORS.white};
+  margin: 8px;
+  padding: 4rem;
+
+  & > h1 {
+    font-size: 2.3rem;
+    color: ${COLORS.text};
+    font-weight: 400;
+  }
+
+  & > p {
+    font-size: 1rem;
+    color: ${COLORS.text};
+    font-weight: 400;
+    margin: 1rem 0 2rem 0;
+  }
+`;
+
+export const CompleteImage = styled.img`
+  max-width: 5rem;
+  margin-bottom: 1.8rem;
+`;
